be75b8c233bf9af9f169edbb0a3fff855fffe908
Ericson Willians <ericson.willians@concil.com.br>
2019-08-26 13:31:46 -0300

    Applied basic rotation

--- Board.gd
+++ Board.gd
@@ -4,9 +4,10 @@ var _timer = null
 const CELL_SIZE = 16;
 const X_CENTER = 80;
 
-onready var boardNode = get_node("/root/Main/Board").get_node("TileMap");
-onready var pieceNode = get_node("/root/Main/Board/IPiece")
+onready var board_node = get_node("/root/Main/Board").get_node("TileMap");
+onready var piece_node = get_node("/root/Main/Board/IPiece")
 var movement = Vector2()
+var current_rotation = 0
 
 func _ready():
 	_timer = Timer.new();
@@ -16,21 +17,32 @@ func _ready():
 	_timer.set_wait_time(1.0)
 	_timer.set_one_shot(false) # Make sure it loops
 	_timer.start()
-	pieceNode.show()
-	pieceNode.position.x = X_CENTER;
-	pieceNode.position.y = CELL_SIZE;
+	piece_node.show()
+	piece_node.position.x = X_CENTER
+	piece_node.position.y = CELL_SIZE
 
 func _input(event):
-    if event is InputEventKey and event.pressed:
-        if event.scancode == KEY_RIGHT:
-            pieceNode.position.x = pieceNode.position.x + CELL_SIZE;
-        elif event.scancode == KEY_LEFT:
-             pieceNode.position.x = pieceNode.position.x - CELL_SIZE;
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_RIGHT:
+			piece_node.position.x = piece_node.position.x + CELL_SIZE
+		elif event.scancode == KEY_LEFT:
+			piece_node.position.x = piece_node.position.x - CELL_SIZE
+		elif event.scancode == KEY_UP:
+			var rotations = piece_node.get_children()
+			if (current_rotation < rotations.size() - 1):
+				current_rotation = current_rotation + 1
+			else:
+				current_rotation = 0
+			for i in range(0, rotations.size()):
+				if (i == current_rotation):
+					rotations[i].show()
+				else:
+					rotations[i].hide()
 
 func _on_Timer_timeout():
-	var lastLineYPos = boardNode.map_to_world(Vector2(0, 21))[1];
-	if (pieceNode.position.y < lastLineYPos - CELL_SIZE):
-		pieceNode.position.y = pieceNode.position.y + CELL_SIZE;
+	var last_line_y_pos = board_node.map_to_world(Vector2(0, 21))[1];
+	if (piece_node.position.y < last_line_y_pos - CELL_SIZE):
+		piece_node.position.y = piece_node.position.y + CELL_SIZE;
 
 func _process(delta):
 	pass
